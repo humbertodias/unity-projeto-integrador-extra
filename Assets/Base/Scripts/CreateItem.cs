6bdd60157eba0f977b41a06e565a3dbff854e3be
Humberto Dias <hldtux@gmail.com>
2017-02-26 17:04:28 -0300

    Fase01

---
+++ Assets/Base/Scripts/CreateItem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateItem : MonoBehaviour {
+
+	// This is the casle prefab, to be set in the inspector
+	public GameObject prefab;
+
+	// This holds the game-world instance of the prefab
+	GameObject instance;
+
+	void Update() {
+		// Is the player currently selectin a place to build the castle? Or in
+		// other words, was the instance variable set?
+		if (instance != null) {
+			// Find out the 3D world coordinates under the cursor
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				if (hit.transform.name == "Ground") {
+					// Refresh the instance position
+					instance.transform.position = hit.point;
+
+					// Note: if your castle appears to be 'in' the Ground
+					//       instead of 'on' the ground, you may have to adjust
+					//       the y coordinate like so:
+					//instance.transform.position += new Vector3(0, 1.23f, 0);
+				}
+			}
+
+			// Player clicked? Then stop positioning the castle by simply
+			// loosing track of our instance. Its still in the game world after-
+			// wards, but we just can't position it anymore.
+			if (Input.GetMouseButton(0)) {
+				instance = null;
+			}
+		}
+	}
+
+	public void Create() {
+			// Instantiate the prefab and keep track of it by assigning it to
+			// our instance variable.
+			instance = (GameObject)GameObject.Instantiate(prefab);
+	}
+
+}
---
+++ Assets/Base/Scripts/HUD.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnGUI() {
+		GUILayout.BeginArea(new Rect(Screen.width-30,
+			Screen.height - 30,
+			30,
+			30), "", "box");
+
+		string buttonText = GameObject.FindGameObjectsWithTag("Npc").Length.ToString();
+
+		if (GUILayout.Button(buttonText)) {
+		}
+		GUILayout.EndArea();
+
+	
+		GUILayout.BeginArea(new Rect(Screen.width-60,
+			Screen.height - 30,
+			30,
+			30), "", "box");
+
+		buttonText = GameObject.FindGameObjectsWithTag("Player").Length.ToString();
+
+		if (GUILayout.Button(buttonText)) {
+		}
+		GUILayout.EndArea();
+	
+	}
+
+}
