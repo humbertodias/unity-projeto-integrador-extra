fc3fd9f2289a62aec41d33317d50c67e35af21a4
Humberto Dias <hldtux@gmail.com>
2017-02-27 09:43:48 -0300

    Parametrizando número máximo de NPCs

--- Assets/Base/Scripts/CastleNpc.cs
+++ Assets/Base/Scripts/CastleNpc.cs
@@ -5,16 +5,23 @@ public class CastleNpc : MonoBehaviour {
     // Parameters
     public float spawnInterval = 3.0f;
     float spawnIntervalElapsed = 0.0f;
+	public int spawnMaxNpcs = 15;
 
     void Update() {
-        // Build it every few seconds
-        spawnIntervalElapsed += Time.deltaTime;
-        if (spawnIntervalElapsed >= spawnInterval) {
-            // Use UnitSpawner
-            GetComponent<UnitSpawner>().spawn();
 
-            // Reset interval
-            spawnIntervalElapsed = 0.0f;
-        }
+		int npcs = GameObject.FindGameObjectsWithTag ("Npc").Length;
+		if (npcs <= spawnMaxNpcs) {
+			
+			// Build it every few seconds
+			spawnIntervalElapsed += Time.deltaTime;
+			if (spawnIntervalElapsed >= spawnInterval) {
+				// Use UnitSpawner
+				GetComponent<UnitSpawner> ().spawn ();
+
+				// Reset interval
+				spawnIntervalElapsed = 0.0f;
+			}
+
+		}
     }
 }
