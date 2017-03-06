889e1f2d8cf8611e6fd875a3d423bffa0ed75c40
Humberto Dias <hldtux@gmail.com>
2017-03-05 22:21:34 -0300

    Música de fundo para cena de créditos

--- Assets/Base/Scripts/UnitSpawner.cs
+++ Assets/Base/Scripts/UnitSpawner.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 using System.Collections;
 
 public class UnitSpawner : MonoBehaviour {
-    // unit prefab
+    // prefab
     public GameObject unit;
     public float spawnRange = 1.5f;
   
     public void spawn() {
-        // start new animation
+        // inicia nova animação
         GetComponent<AnimationSinus>().toggle();
         
-        // create a new unit at some random position around this place
+        // cria uma nova unidade em uma posição aleatória
         Vector3 pos = transform.position;
         float x = pos.x + Random.Range(-1.0f, 1.0f) * spawnRange;
         float y = pos.y;
